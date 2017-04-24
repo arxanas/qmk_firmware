44fc55dbae058ead13ea7ad5ba4ebbdbf02a3bc8
Waleed Khan <me@waleedkhan.name>
2017-04-24 16:26:31 -0700

    Collapse symbol layer into media layer
    I don't do data entry, so most of the symbol layer is useless for me.
    The only times that I ever enter it are to use the function keys, which
    are few. I also accidentally enable symbol layer all the time, so it
    would be better if I could use a different key for its functionality.

--- keyboards/ergodox/ez/keymaps/plover-arxanas/keymap.c
+++ keyboards/ergodox/ez/keymaps/plover-arxanas/keymap.c
@@ -5,9 +5,8 @@
 #include "virtser.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
-#define MDIA 2 // media keys
-#define PLVR 3 // Plover layer
+#define MDIA 1 // media keys
+#define PLVR 2 // Plover layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer (Dvorak)
@@ -34,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(
         // left hand
         KC_NO,          KC_1,     KC_2,     KC_3,    KC_4,     KC_5,  KC_NO,
-        KC_TAB,         KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,     KC_Y,  KC_FN21,
+        KC_TAB,         KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,     KC_Y,  KC_NO,
         CTL_T(KC_ESC),  KC_A,     KC_O,     KC_E,    KC_U,     KC_I,
         KC_LSFT,        KC_SCLN,  KC_Q,     KC_J,    KC_K,     KC_X,  KC_LGUI,
         KC_GRV,         KC_NO,    KC_NO,    KC_NO,   KC_LBRC,
@@ -51,49 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PGUP,
              KC_PGDN,  KC_ENT,  KC_SPC
     ),
-/* Keymap 1: Symbol Layer
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | TEENSY |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |    . |   0  |   =  |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-// SYMBOLS
-[SYMB] = KEYMAP(
-       // left hand
-       RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
-       // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
-),
-/* Keymap 2: Media and mouse keys
+/* Keymap 1: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
@@ -116,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 KEYMAP(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -125,7 +82,7 @@ KEYMAP(
                                                     KC_TRNS,
                                   KC_BTN1, KC_BTN2, KC_TRNS,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
@@ -165,7 +122,7 @@ KEYMAP(
 #define GRP2 0b10000000
 #define GRP3 0b11000000
 
-/* Keymap 3: Steno for Plover from https://github.com/shayneholmes/tmk_keyboard/commit/11290f8489013018f778627db725160c745e75bd
+/* Keymap 2: Steno for Plover from https://github.com/shayneholmes/tmk_keyboard/commit/11290f8489013018f778627db725160c745e75bd
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
@@ -187,7 +144,7 @@ KEYMAP(
  *                                 `--------------------'       `--------------------'
  */
 
-[PLVR] = KEYMAP(  // layout: layer 3: Steno for Plover
+[PLVR] = KEYMAP(  // layout: layer 2: Steno for Plover
         // left hand
         KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_TRNS,
         KC_TRNS,  M(NM),  M(NM),  M(NM),  M(NM),  M(NM),  KC_TRNS,
@@ -210,8 +167,7 @@ KEYMAP(
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [21] = ACTION_LAYER_TAP_TOGGLE(SYMB), // FN21 - Momentary layer 1 (Symbols)
-    [22] = ACTION_LAYER_TAP_TOGGLE(MDIA), // FN22 - Momentary layer 2 (Media)
+    [21] = ACTION_LAYER_TAP_TOGGLE(MDIA), // FN21 - Momentary layer 1 (Media)
 };
 
 uint8_t chord[4] = {0, 0, 0, 0};
@@ -268,9 +224,6 @@ void matrix_scan_user(void) {
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
     switch (layer) {
-        case SYMB:
-            ergodox_right_led_1_on();
-            break;
         case MDIA:
             ergodox_right_led_2_on();
             break;
